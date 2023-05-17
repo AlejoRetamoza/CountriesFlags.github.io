6be54e205447936dede67c83b4103828d86a1162
Alejo Retamoza <alejoretamoza72@gmail.com>
2023-05-16 21:19:21 -0300

    Primera version api de los paises

---
+++ main.js
@@ -0,0 +1,33 @@
+function getCountries(done){
+
+    const results = fetch('https://restcountries.com/v3.1/all');
+
+    results 
+        .then (response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+getCountries(data => {
+    data.forEach(country => {
+
+        const article = document.createRange().createContextualFragment(`
+        
+        <article>
+
+        <div class="img-container">
+            <img src="${country.flags.png}" alt="">
+        </div>
+
+        <div class="info-container">s
+            <h3>${country.name.common}</h3>
+            <span>${country.capital}</span>
+        </div>
+        </article>
+        `)
+        const main = document.querySelector('main')
+
+        main.append(article)
+    });
+});
